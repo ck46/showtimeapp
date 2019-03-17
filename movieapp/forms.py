716d770bb42913b0dc60281bdc6718a10aecf523
sjtg <chipilej@gmail.com>
2019-03-17 09:39:17 +0200

    Added Content

--- movieapp/forms.py
+++ movieapp/forms.py
@@ -3,11 +3,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import *
 
+
+class DirectorForm(forms.ModelForm):
+    
+    class Meta:
+        model = Director
+        fields = ['directorName', 'directorImages']
+
+
+class ActorForm(forms.ModelForm):
+    
+    class Meta:
+        model = Actor
+        fields = ['description', 'mainActorImages','actorOne', 'secondActorImages','actorTwo', 'thirdActorImages','actorThree', 'fourthActorImages', 'actorFour']
+
+
 class MoviesForm(forms.ModelForm):
 
     class Meta:
         model = Movie
-        fields = ['title', 'genre', 'description', 'publishedDate', 'movieImages', 'movieVideo']
+        fields = ['title', 'genre', 'description','directors', 'actors', 'publishedDate', 'movieImages', 'movieVideo']
         
 
 
--- movieapp/models.py
+++ movieapp/models.py
@@ -21,8 +21,44 @@ class Genre(models.Model):
         return title 
 
 
+# Director
+class Director(models.Model):
+    directorName  = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    publishedDate = models.IntegerField(
+        blank=True, null=True
+    )
+    directorImages = models.FileField(upload_to='Movie/DirectorImage/', blank=False, null=True)
+
+    def __unicode__(self):
+        directorName = str(self.directorName)
+        return directorName 
+
+
+# Actors 
+class Actor(models.Model):
+    description = models.CharField(max_length=200)
+    publishedDate = models.IntegerField(
+        blank=True, null=True
+    )
+    mainActorImages = models.FileField(upload_to='Movie/ActorsImage/', blank=False, null=True)
+    actorOne = models.CharField(max_length=200)
+    secondActorImages = models.FileField(upload_to='Movie/ActorsImage/', blank=False, null=True)
+    actorTwo = models.CharField(max_length=200)
+    thirdActorImages = models.FileField(upload_to='Movie/ActorsImage/', blank=False, null=True)
+    actorThree = models.CharField(max_length=200)
+    fourthActorImages = models.FileField(upload_to='Movie/ActorsImage/', blank=False, null=True)
+    actorFour = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        description = str(self.description)
+        return description
+
+
 # Movie  
 class Movie(models.Model):
+    directors = models.ForeignKey(Director)
+    actors = models.ForeignKey(Actor)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     genre = models.ForeignKey(Genre)
     title = models.CharField(max_length=200)
@@ -39,35 +75,6 @@ class Movie(models.Model):
         return title 
 
 
-# Director
-class Director(models.Model):
-    movies = models.ForeignKey(Movie)
-    directorName  = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
-    publishedDate = models.IntegerField(
-        blank=True, null=True
-    )
-    directorImages = models.FileField(upload_to='Movie/DirectorImage/', blank=False, null=True)
 
-    def __unicode__(self):
-        directorName = str(self.directorName)
-        return directorName 
-
-
-# Actors 
-class Actor(models.Model):
-    movies = models.ForeignKey(Movie)
-    description = models.CharField(max_length=200)
-    publishedDate = models.IntegerField(
-        blank=True, null=True
-    )
-    mainActorImages = models.FileField(upload_to='Movie/ActorsImage/', blank=False, null=True)
-    secondActorImages = models.FileField(upload_to='Movie/ActorsImage/', blank=False, null=True)
-    thirdActorImages = models.FileField(upload_to='Movie/ActorsImage/', blank=False, null=True)
-    fourthActorImages = models.FileField(upload_to='Movie/ActorsImage/', blank=False, null=True)
-
-    def __unicode__(self):
-        description = str(self.description)
-        return description
 
 
